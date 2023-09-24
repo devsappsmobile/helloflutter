f27a6e88b963bb6c2a4e36bc0b31f6aea9f09c3f
JesCan <jesuscanales@outlook.com>
2023-09-24 12:13:49 -0500

    Ya tenemos la primera versión de nuestra intro

--- start_screen_apps/lib/presentation/screens/basic_intro.dart
+++ start_screen_apps/lib/presentation/screens/basic_intro.dart
@@ -5,12 +5,44 @@ class BasicIntro extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-          child: Text(
-        'Soy una nueva pantalla',
-        style: (TextStyle(fontSize: 30, color: Colors.blue)),
-      )),
-    );
+    return Scaffold(
+        backgroundColor: const Color(0xFFFDE8CD),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'INSTITUTO',
+                style: TextStyle(fontSize: 20),
+              ),
+              Text(
+                'VALLE GRANDE',
+                style: TextStyle(
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue.shade700,
+                ),
+              ),
+              Text(
+                'Perfeccionamos tu talento...',
+                style: TextStyle(
+                    fontSize: 25,
+                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.amber.shade800),
+              ),
+              const SizedBox(height: 50),
+              MaterialButton(
+                onPressed: () {},
+                color: Colors.blue.shade700,
+                textColor: Colors.white,
+                padding: const EdgeInsets.all(20),
+                shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20))),
+                child: const Text('Empieza ahora...'),
+              ),
+            ],
+          ),
+        ));
   }
 }
